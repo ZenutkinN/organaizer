50e325f338700129fce09950237882b491e68b21
ZenutkinN <nvzenutkin@gmail.com>
2020-09-13 22:14:28 +0700

    Updates

---
+++ precache-manifest.008b133ec64f662b0fdc395e93aaee3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4118483421e6f05d7846a15a8da3f6",
+    "url": "/learning.organaizer/index.html"
+  },
+  {
+    "revision": "21345253ba9e320cdfc7",
+    "url": "/learning.organaizer/static/css/main.50296d38.chunk.css"
+  },
+  {
+    "revision": "1361f852a6d9f48f489f",
+    "url": "/learning.organaizer/static/js/2.1705f042.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/learning.organaizer/static/js/2.1705f042.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21345253ba9e320cdfc7",
+    "url": "/learning.organaizer/static/js/main.41985304.chunk.js"
+  },
+  {
+    "revision": "eb4b81d1441e141667ef",
+    "url": "/learning.organaizer/static/js/runtime-main.e9a2a14c.js"
+  }
+]);
